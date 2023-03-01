75e9c947b14ec68a3d665af399c0673ff1b51866
SolidityDev <AttalyaCallo@bwg-io.site>
2023-03-01 18:06:29 +0300

    push loadens

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "node": "current"
+        }
+      }
+    ]
+  ],
+  "plugins": [
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
   ]
 }
+
